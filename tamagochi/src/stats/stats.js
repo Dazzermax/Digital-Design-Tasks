a9aef22fec4ac5ef77e0055434d5c815c4410b7c
Max Koshelev <dazzermax1996@gmail.com>
2020-07-01 14:40:12 +0400

    create-react-app, trying react

---
+++ tamagochi/src/stats/stats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+    <div className="progress-bar">
+        <span className="progress-bar__title">Здоровье</span>
+        <div className="progress-bar__container">
+            <div className="progress-bar__parameter progress-bar__parameter-health"></div>
+        </div>
+    </div>
+)
