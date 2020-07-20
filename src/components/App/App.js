315d05de61db0051b5031129643b6a93be5ec944
Max Koshelev <dazzermax1996@gmail.com>
2020-07-20 13:39:15 +0400

    start creating components

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Navigation from '../Navigation/Navigation'
+
+function App() {
+  return (
+    <div className="app">
+     <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+        <aside className="navigation">
+            Навигация
+        </aside>
+    )
+}
+
+export default Navigation;
